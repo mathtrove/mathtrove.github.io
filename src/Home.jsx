b30133bb2d6fecb1f35b3c644daf7f76a5cec22e
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-10-15 10:04:42 +0500

    vite deploy

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+const Home = () =>{
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
