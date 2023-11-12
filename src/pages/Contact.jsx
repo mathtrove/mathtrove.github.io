30f680626972fab24ef6c122be23f23c0cf890b5
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-11-12 21:22:13 +0500

    idk

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Contact.css';
 const Contact = () => {
+
+
     return(
         <>
         <section id="contact">
@@ -8,20 +10,20 @@ const Contact = () => {
                 <h1>Join the Waitlist</h1>
                 <h3>Be the first to get notified when our Website gets completed.</h3>
             </div>
-            <form action=""  method="post" id="frvjne57huhvbuthi86ibjfg">
+            <form onSubmit={e => e.preventDefault}  method="post" id="frvjne57huhvbuthi86ibjfg">
                 <div className="input-box">
-                <input className="input-field" type="text" name="name"/><label htmlFor="name">Name</label>
+                <input className="input-field " id="name" type="text" name="name" /><label htmlFor="name">Name</label>
                 </div>
                 <div className="input-box">
-                <input className="input-field" type="email" name="email"/><label htmlFor="email">Email Address</label>
+                <input className="input-field" id="email" type="email" name="email"/><label htmlFor="email">Email Address</label>
                 </div>
                 <div className="input-box">
-                <input className="input-field" type="text" name="country"/><label htmlFor="country">Country</label>
+                <input className="input-field" id="country" type="text" name="country"/><label htmlFor="country">Country</label>
                 </div>
                 <div className="input-box">
-                <textarea className="input-field" name="suggestion" ></textarea><label htmlFor="suggestion">Suggestions (optional)</label>
+                <textarea className="input-field" id="suggestion" name="suggestion"></textarea><label htmlFor="suggestion">Suggestions (optional)</label>
                 </div>
-                <input onClick={(e) => e.preventDefault} type="submit" className="cta" disabled></input>
+                <input type="submit" className="cta" ></input>
             </form>
         </section>
         </>
