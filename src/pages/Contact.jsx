dffb1da4229a87d365ff594160b1c032c7aa98cd
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-11-16 19:28:08 +0500

    form fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import './Contact.css';
 const Contact = () => {
+
+    const [nameInput, setNameInput] = useState("");
+    const [emailInput, setEmailInput] = useState("");
+    const [countryInput, setCountryInput] = useState("");
+    const [msgInput, setMsgInput] = useState("");
+
+    const submitForm = (e)=>{
+        e.preventDefault();
+        setNameInput("");
+        setEmailInput("");
+        setCountryInput("");
+        setMsgInput("");
+    }
+
+    const changeName = (e)=>{
+        setNameInput(e.target.value);
+    }
+    const changeEmail = (e)=>{
+        setEmailInput(e.target.value);
+    }
+    const changeCountry = (e)=>{
+        setCountryInput(e.target.value);
+    }
+    const changeMsg = (e)=>{
+        setMsgInput(e.target.value);
+    }
 
 
     return(
@@ -10,18 +36,18 @@ const Contact = () => {
                 <h1>Join the Waitlist</h1>
                 <h3>Be the first to get notified when our Website gets completed.</h3>
             </div>
-            <form onSubmit={e => e.preventDefault}  method="post" id="frvjne57huhvbuthi86ibjfg">
+            <form onSubmit={submitForm}  method="post" id="frvjne57huhvbuthi86ibjfg">
                 <div className="input-box">
-                <input className="input-field " id="name" type="text" name="name" /><label htmlFor="name">Name</label>
+                <input className="input-field " id="name" type="text" name="name" value={nameInput} onChange={changeName} /><label htmlFor="name">Name</label>
                 </div>
                 <div className="input-box">
-                <input className="input-field" id="email" type="email" name="email"/><label htmlFor="email">Email Address</label>
+                <input className="input-field" id="email" type="email" name="email" value={emailInput} onChange={changeEmail}/><label htmlFor="email">Email Address</label>
                 </div>
                 <div className="input-box">
-                <input className="input-field" id="country" type="text" name="country"/><label htmlFor="country">Country</label>
+                <input className="input-field" id="country" type="text" name="country" value={countryInput} onChange={changeCountry}/><label htmlFor="country">Country</label>
                 </div>
                 <div className="input-box">
-                <textarea className="input-field" id="suggestion" name="suggestion"></textarea><label htmlFor="suggestion">Suggestions (optional)</label>
+                <textarea className="input-field" id="suggestion" name="suggestion" value={msgInput} onChange={changeMsg}></textarea><label htmlFor="suggestion">Suggestions (optional)</label>
                 </div>
                 <input type="submit" className="cta" ></input>
             </form>
